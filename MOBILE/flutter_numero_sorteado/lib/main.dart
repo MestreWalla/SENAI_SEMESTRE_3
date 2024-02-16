24b3eafc6a4999c52b587b7972ab72c5e4c9439a
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-02-15 21:57:59 -0300

    Update main.dart

--- MOBILE/flutter_numero_sorteado/lib/main.dart
+++ MOBILE/flutter_numero_sorteado/lib/main.dart
@@ -1,20 +1,81 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(NumeroAleatorioApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class NumeroAleatorioApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: NumeroAleatorio(),
+    );
+  }
+}
+
+class NumeroAleatorio extends StatefulWidget {
+  @override
+  _NumeroAleatorio createState() => _NumeroAleatorio();
+}
+
+class _NumeroAleatorio extends State<NumeroAleatorio> {
+  TextEditingController _NumeroDigitado = TextEditingController();
+  String _resultado = '';
+  int numeroSorteado = 0;
+  int minimo = 1; // Intervalo mínimo
+  int maximo = 10; // Intervalo máximo
+
+
+  void _sortear(String operacao) {
+    int palpite = int.tryParse(_NumeroDigitado.text) ?? 0;
+
+    setState(() {
+      numeroSorteado = Random().nextInt(maximo - minimo + 1) + minimo;
+      _resultado = palpite == numeroSorteado
+          ? "Parabéns! Você acertou!"
+          : "Infelizmente, você errou. O número sorteado era $numeroSorteado.";
+      _NumeroDigitado.text = ""; // Limpa o campo de texto após a tentativa
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Calculadora Flutter'),
         ),
-      ),
-    );
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextField(
+                  controller: _NumeroDigitado,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: 'Digite o numero que voce acha que é'),
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: () => _sortear('Sortear'),
+                      child: Text('Sortear'),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 139, 164, 254)),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                Text(_resultado,
+                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+              ],
+            )));
   }
 }
