dd38ef94a1edc0ac4176da3afbbbab32b7c5290e
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-02-16 19:25:24 -0300

    Update main.dart

--- MOBILE/flutter_numero_sorteado/lib/main.dart
+++ MOBILE/flutter_numero_sorteado/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,17 +25,26 @@ class _NumeroAleatorio extends State<NumeroAleatorio> {
   int numeroSorteado = 0;
   int minimo = 1; // Intervalo mínimo
   int maximo = 10; // Intervalo máximo
+  int tentativas = 0;
 
-
-  void _sortear(String operacao) {
+  void _verificar(String operacao) {
     int palpite = int.tryParse(_NumeroDigitado.text) ?? 0;
 
     setState(() {
-      numeroSorteado = Random().nextInt(maximo - minimo + 1) + minimo;
+      tentativas++;
       _resultado = palpite == numeroSorteado
-          ? "Parabéns! Você acertou!"
+          ? "Parabéns! Você acertou na tentativa de numero $tentativas!"
           : "Infelizmente, você errou. O número sorteado era $numeroSorteado.";
-      _NumeroDigitado.text = ""; // Limpa o campo de texto após a tentativa
+      _NumeroDigitado.text = ""; // Limpar o campo de texto após a tentativa
+    });
+  }
+
+  void _sortear(String operacao) {
+    setState(() {
+      numeroSorteado = Random().nextInt(maximo - minimo + 1) + minimo;
+      _resultado = "";
+      tentativas = 0;
+      _NumeroDigitado.text = "";
     });
   }
 
@@ -44,7 +52,7 @@ class _NumeroAleatorio extends State<NumeroAleatorio> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Calculadora Flutter'),
+          title: Text('Adivinhação de número Flutter'),
         ),
         body: Padding(
             padding: const EdgeInsets.all(16.0),
@@ -55,13 +63,23 @@ class _NumeroAleatorio extends State<NumeroAleatorio> {
                 TextField(
                   controller: _NumeroDigitado,
                   keyboardType: TextInputType.number,
-                  decoration: InputDecoration(labelText: 'Digite o numero que voce acha que é'),
+                  decoration: InputDecoration(
+                      labelText:
+                          'Digite o numero que voce acha que é entre $minimo e $maximo.'),
                 ),
                 SizedBox(height: 16.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: () => _verificar('Verificar'),
+                      child: Text('Verificar'),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 139, 254, 218)),
+                      ),
+                    ),
                     ElevatedButton(
                       onPressed: () => _sortear('Sortear'),
                       child: Text('Sortear'),
@@ -74,7 +92,8 @@ class _NumeroAleatorio extends State<NumeroAleatorio> {
                 ),
                 SizedBox(height: 16.0),
                 Text(_resultado,
-                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
               ],
             )));
   }
