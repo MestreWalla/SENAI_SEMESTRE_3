7ac62b870eb945c2d6f43901f524ff95be8259ae
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-02-22 20:56:44 -0300

    Update main.dart

--- MOBILE/app_todolist_02_22/lib/main.dart
+++ MOBILE/app_todolist_02_22/lib/main.dart
@@ -1,5 +1,7 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:app_todolist_02_22/tarefas_app.dart';
+
 
 void main() {
-  // runApp();
+  runApp(TarefasApp());
 }
