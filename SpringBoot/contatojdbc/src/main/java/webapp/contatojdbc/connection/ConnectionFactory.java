76c5a4be34acb16b01f533f2ff61301669ae1fb5
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-03-06 21:52:59 -0300

    update

--- SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/connection/ConnectionFactory.java
+++ SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/connection/ConnectionFactory.java
@@ -2,27 +2,40 @@ package webapp.contatojdbc.connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
     private static final String url = "jdbc:postgresql://localhost:5432/maycon";
-    private static final String user = "postgres";
-    private static final String password = "<postgres>";
+    private static final String usuario = "postgres"; // Nome do ADM do banco
+    private static final String senha = "postgres"; // Senha do ADM do banco
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao obter conecção com o banco de dados: " + e.getMessage());
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao obter conexão com o banco de dados.", e);
         }
     }
-    public static void closeConnection(Connection connection) {
+     public static void closeConnection(Connection connection) {
         try {
             if (connection != null) {
                 connection.close();
-            } 
+            }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
+    // Método para fechar a conexão e o objeto PreparedStatement
+    public static void closeConnection(Connection connection, PreparedStatement stmt) {
+        closeConnection(connection);
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
--- SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/controller/IndexController.java
+++ SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/controller/IndexController.java
@@ -1,5 +1,32 @@
 package webapp.contatojdbc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import webapp.contatojdbc.connection.EmailDAO;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
 public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView abrirIndex() {
+        ModelAndView mv = new ModelAndView("index");
+
+        String mensagem = "Olá, seja bem-vinda(o)!";
+        mv.addObject("msg", mensagem);
+        new EmailDAO().criaTabela();
+        return mv;
+    }
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ModelAndView buscarIndex(@RequestParam("email") String email) {
+        ModelAndView mv = new ModelAndView("index");
+
+        new EmailDAO().cadastrar(email);
+
+        return mv;
+    }
     
 }
