e89ef67bc38ed348ead5951177bebaa1d48280ea
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-03-06 22:00:32 -0300

    update

--- SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/controller/IndexController.java
+++ SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/controller/IndexController.java
@@ -7,26 +7,36 @@ import org.springframework.web.servlet.ModelAndView;
 import webapp.contatojdbc.connection.EmailDAO;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private EmailDAO emailDAO; // Injeção do EmailDAO
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView abrirIndex() {
         ModelAndView mv = new ModelAndView("index");
 
-        String mensagem = "Olá, seja bem-vinda(o)!";
+        String mensagem = "Olá, seja bem-vindo(a)!";
         mv.addObject("msg", mensagem);
-        new EmailDAO().criaTabela();
+
+        List<String> emails = emailDAO.buscarEmails(); // Método para buscar os emails
+        mv.addObject("emails", emails);
+
         return mv;
     }
+
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public ModelAndView buscarIndex(@RequestParam("email") String email) {
         ModelAndView mv = new ModelAndView("index");
 
-        new EmailDAO().cadastrar(email);
+        emailDAO.cadastrar(email); // Chamada do método para cadastrar o email
 
         return mv;
     }
-    
 }
