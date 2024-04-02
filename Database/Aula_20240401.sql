93665c3b1ddf0c191157e6a147b5b505bd566c14
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-04-01 21:52:25 -0300

    Create Aula_20240401.sql

---
+++ Database/Aula_20240401.sql
@@ -0,0 +1,95 @@
+--EXERCICIO_01 - CRIAÇÃO DAS TABELAS
+
+-- Criação do Banco de Dados
+CREATE DATABASE Exercicios_Aula;
+
+-- Utilização do Banco de Dados criado
+USE Exercicios_Aula;
+
+-- Criação das Tabelas
+CREATE TABLE Fornecedor (
+    Fcodigo INT PRIMARY KEY,
+    Fnome VARCHAR(255),
+    Status VARCHAR(20) DEFAULT 'Ativo',
+    Cidade VARCHAR(100)
+);
+
+CREATE TABLE Peca (
+    Pcodigo INT PRIMARY KEY,
+    Pnome VARCHAR(255) NOT NULL,
+    Cor VARCHAR(50),
+    Peso INT,
+    Cidade VARCHAR(100)
+);
+
+CREATE TABLE Instituicao (
+    Icodigo INT PRIMARY KEY,
+    nome VARCHAR(255)
+);
+
+CREATE TABLE Projeto (
+    PRcod INT PRIMARY KEY,
+    PRnome VARCHAR(255),
+    Cidade VARCHAR(100),
+    Icod INT,
+    FOREIGN KEY (Icod) REFERENCES Instituicao(Icodigo)
+);
+
+CREATE TABLE Fornecimento (
+    Fcod INT PRIMARY KEY,
+    Fcod INT,
+    Pcod INT,
+    PRcod INT,
+    Quantidade INT,
+    FOREIGN KEY (Fcod) REFERENCES Fornecedor(Fcodigo),
+    FOREIGN KEY (Pcod) REFERENCES Peca(Pcodigo),
+    FOREIGN KEY (PRcod) REFERENCES Projeto(PRcod)
+);
+
+--EXERCICIO_02 - ALTERAÇÃO DAS TABELAS
+
+ALTER TABLE Fornecedor RENAME COLUMN Fcodigo TO Fcod;
+ALTER TABLE Fornecedor ADD COLUMN Fone VARCHAR(20);
+ALTER TABLE Fornecedor ADD COLUMN Ccod INT;
+
+CREATE TABLE Cidade (
+    Ccod INT PRIMARY KEY,
+    Cnome VARCHAR(100),
+    uf VARCHAR(2)
+);
+
+ALTER TABLE Peca RENAME COLUMN Pcodigo TO Pcod;
+ALTER TABLE Peca ADD COLUMN Ccod INT;
+ALTER TABLE Projeto RENAME COLUMN PRcod TO PRcod;
+ALTER TABLE Projeto ADD COLUMN Ccod INT;
+ALTER TABLE Fornecimento RENAME COLUMN Fcod TO Fcod;
+
+--EXERCICIO_03 - INSERÇÃO DE DADOS
+
+INSERT INTO Fornecedor (Fcod, Fnome, Status, Fone, Ccod) VALUES
+(01, 'Kalunga', 'Ativo', '1930234066', 11),
+(02, 'LLC Logística', 'Inativo', '1130245578', 12),
+(03, 'Velho Barreiro', 'Ativo', '1930324068', 13),
+(04, 'Hyundai', 'Ativo', '1930224545', 14);
+
+INSERT INTO Cidade (Ccod, Cnome, uf) VALUES
+(11, 'Limeira', 'SP'),
+(12, 'São Paulo', 'SP'),
+(13, 'Rio Claro', 'SP'),
+(14, 'Piracicaba', 'SP');
+
+INSERT INTO Peca (Pcod, Pnome, Cor, Peso, Ccod) VALUES
+(0001, 'Processador', 'Padrão', 300, 13),
+(0002, 'Lona Azul', NULL, 12, 11),
+(0003, 'Cachaça Transparente', NULL, NULL, 11),
+(0004, 'Hyundai Creta', NULL, 100, 10000);
+
+INSERT INTO Projeto (PRcod, PRnome, Ccod) VALUES
+(01, 'Eletrônicos', 11),
+(02, 'Mecânicos', 13),
+(03, 'Escolas', 12);
+
+INSERT INTO Fornecimento (Fcod, Pcod, PRcod, Quantidade) VALUES
+(01, 0001, 01, 660),
+(02, 0002, 03, 10),
+(04, 0004, 02, 220);
