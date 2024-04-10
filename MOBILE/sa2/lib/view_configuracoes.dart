b88afcb76fb39a0931008700ad86ab8b0bc84efd
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-04-09 21:54:44 -0300

    Update view_configuracoes.dart

--- MOBILE/sa2/lib/view_configuracoes.dart
+++ MOBILE/sa2/lib/view_configuracoes.dart
@@ -1,6 +1,6 @@
 // Arquivo view_configuracoes.dart
 
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
@@ -53,6 +53,62 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
             ],
           );
         },
+      ),
+    );
+  }
+}
+
+class ConfiguracoesUsuarioPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Configurações de Usuário'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Nome de Usuário:',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Digite seu nome de usuário',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+              'Email:',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Digite seu email',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+              'Alterar Senha:',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Digite sua nova senha',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Implemente a lógica para salvar as configurações do usuário
+              },
+              child: const Text('Salvar'),
+            ),
+          ],
+        ),
       ),
     );
   }
